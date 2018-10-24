6514115bdafad20d2b5f48a3a3d847731f347793
cs947 <cs947@cornell.edu>
2018-10-24 11:45:51 -0400

    Updaed Start.py

--- Start.py
+++ Start.py
@@ -5,12 +5,12 @@ from institutions import find_inst_info
 
 control = "continue"
 info = "Choose command from following list\n"\
-       "-- all : Executes all of the below commands\n"
+       "-- all : Executes all of the below commands\n" \
        "-- binfo : Generate a spreadsheet of biographical profiles Approx. 5 mins\n" \
-       "-- ainfo : Generate a spreadsheet of Academic Institutions links info Approx 5 mins\n" \
-       "-- noainfo : Generate a spreadsheet of Non Academic Institutions links info Approx 5 mins\n" \
-       "-- appinfo : Generate a spreadsheet of Application Areas links info Approx 5 mins\n" \
-       "-- minfo : Generate a spreadsheet of Methodologies links info Approx 5 mins\n" \
+       "-- ainfo : Generate a spreadsheet of Academic Institutions links info Approx. 5 mins\n" \
+       "-- noainfo : Generate a spreadsheet of Non Academic Institutions links info Approx. 5 mins\n" \
+       "-- appinfo : Generate a spreadsheet of Application Areas links info Approx. 5 mins\n" \
+       "-- minfo : Generate a spreadsheet of Methodologies links info Approx. 5 mins\n" \
        "-- find : Find all the pages under informs. Approx. 10 mins\n" \
        "-- link : Output dead links after 'find' is excuted. Approx. 4 hrs\n" \
        "-- exit : Exit the program\n"
